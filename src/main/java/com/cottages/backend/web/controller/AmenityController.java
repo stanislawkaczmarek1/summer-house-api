0d95e3607e05dbd761157d62b089c661fa64f166
Yauheniya Merkushevich <153443291+zhenia0908@users.noreply.github.com>
2026-06-01 16:15:01 +0200

    .

--- src/main/java/com/cottages/backend/web/controller/AmenityController.java
+++ src/main/java/com/cottages/backend/web/controller/AmenityController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class AmenityController {
     private final AmenityService amenityService;
     public AmenityController(AmenityService amenityService){this.amenityService = amenityService;}
